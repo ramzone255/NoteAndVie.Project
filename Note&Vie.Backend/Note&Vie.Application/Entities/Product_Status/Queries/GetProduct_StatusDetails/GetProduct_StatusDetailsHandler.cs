2d02575543dbfd467761979d77eef55f9e899e1d
Ilya Prokaznikov <ramzone255@mail.ru>
2024-09-30 16:03:26 +0300

    Queries/GetProduct_StatusDetails

---
+++ Note&Vie.Backend/Note&Vie.Application/Entities/Product_Status/Queries/GetProduct_StatusDetails/GetProduct_StatusDetailsHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Note_Vie.Application.Common.Exceptions;
+using Note_Vie.Application.Interfaces;
+using Note_Vie.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note_Vie.Application.Entities.Product_Status.Queries.GetProduct_StatusDetails
+{
+    public class GetProduct_StatusDetailsHandler
+        : IRequestHandler<GetProduct_StatusDetailsQuery, Product_StatusDetailsVm>
+    {
+        private readonly INote_VieDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetProduct_StatusDetailsHandler(INote_VieDbContext dbContext,
+            IMapper mapper) => (_dbContext, _mapper) = (dbContext, mapper);
+        public async Task<Product_StatusDetailsVm> Handle(GetProduct_StatusDetailsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Product_Status
+                .FirstOrDefaultAsync(note =>
+                note.id_product_status == request.id_product_status, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Domain.Entities.Product_Status), request.id_product_status);
+            }
+
+            return _mapper.Map<Product_StatusDetailsVm>(entity);
+        }
+    }
+}
---
+++ Note&Vie.Backend/Note&Vie.Application/Entities/Product_Status/Queries/GetProduct_StatusDetails/Product_StatusDetailsVm.cs
@@ -0,0 +1,26 @@
+﻿using Note_Vie.Application.Common.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Note_Vie.Domain;
+using AutoMapper;
+
+namespace Note_Vie.Application.Entities.Product_Status.Queries.GetProduct_StatusDetails
+{
+    public class Product_StatusDetailsVm : IMapWith<Domain.Entities.Product_Status>
+    {
+        public int id_product_status { get; set; }
+        public string product_status_name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Domain.Entities.Product_Status, Product_StatusDetailsVm>()
+                .ForMember(noteVm => noteVm.id_product_status,
+                opt => opt.MapFrom(note => note.id_product_status))
+                .ForMember(noteVm => noteVm.product_status_name,
+                opt => opt.MapFrom(note => note.product_status_name));
+        }
+    }
+}
